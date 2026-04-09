31e30bdc82f7ef6c951090c8307678ef2c1360e2
mounttrout <mount.trout@gmail.com>
2026-04-09 21:20:42 +0900

    move requirements

--- voyageflow/core/llm_client.py
+++ voyageflow/core/llm_client.py
@@ -11,7 +11,7 @@ import os
 import json
 import time
 from typing import Optional, Dict, Any
-from google import genai
+# from google import genai
 # from google.genai import types 4/9変更　github対策
 import google.generativeai as genai
 from dotenv import load_dotenv
